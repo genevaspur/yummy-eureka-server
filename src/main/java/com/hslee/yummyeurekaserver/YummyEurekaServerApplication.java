5b957b21f0e4ffe1162322c6577027988b9969a0
HakSeong Lee <kth4241@gmail.com>
2023-08-02 10:18:56 +0900

    enable eureka

--- src/main/java/com/hslee/yummyeurekaserver/YummyEurekaServerApplication.java
+++ src/main/java/com/hslee/yummyeurekaserver/YummyEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.hslee.yummyeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class YummyEurekaServerApplication {
 
     public static void main(String[] args) {
