82d6ef0b2f833ef97e173f774b810324a28b3dee
HakSeong Lee <kth4241@gmail.com>
2023-08-02 10:14:35 +0900

    initial commit

---
+++ src/main/java/com/hslee/yummyeurekaserver/YummyEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.hslee.yummyeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YummyEurekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hslee/yummyeurekaserver/YummyEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hslee.yummyeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
